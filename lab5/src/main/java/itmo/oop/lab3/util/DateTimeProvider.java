5d53af5103b8818bd3061544066b75db2528b803
Ilya Kuznetsov <ikuz2002@gmail.com>
2020-12-09 01:35:22 +0300

    feat: Add date change hooks

--- lab5/src/main/java/itmo/oop/lab3/util/DateTimeProvider.java
+++ lab5/src/main/java/itmo/oop/lab3/util/DateTimeProvider.java
@@ -1,7 +1,9 @@
 package itmo.oop.lab3.util;
 
 import java.time.ZonedDateTime;
+import java.util.function.Consumer;
 
 public interface DateTimeProvider {
     ZonedDateTime currentDateTime();
+    void addDateChangeHook(Consumer<ZonedDateTime> hook);
 }
--- lab5/src/main/java/itmo/oop/lab3/util/DefaultDateTimeProvider.java
+++ lab5/src/main/java/itmo/oop/lab3/util/DefaultDateTimeProvider.java
@@ -1,10 +1,45 @@
 package itmo.oop.lab3.util;
 
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class DefaultDateTimeProvider implements DateTimeProvider {
+
+    private final List<Consumer<ZonedDateTime>> hooks = new ArrayList<>();
+
+    @SuppressWarnings("BusyWait")
+    public DefaultDateTimeProvider() {
+        Thread dateWatcher = new Thread(() -> {
+            while (true) {
+                ZonedDateTime previous = currentDateTime();
+
+                while (previous.getDayOfWeek() == currentDateTime().getDayOfWeek()) {
+                    try {
+                        Thread.sleep(3_600_000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                synchronized (hooks) {
+                    for (var hook : hooks)
+                        hook.accept(currentDateTime());
+                }
+            }
+        });
+    }
+
     @Override
     public ZonedDateTime currentDateTime() {
         return ZonedDateTime.now();
+    }
+
+    @Override
+    public void addDateChangeHook(Consumer<ZonedDateTime> hook) {
+        synchronized (hooks) {
+            hooks.add(hook);
+        }
     }
 }
