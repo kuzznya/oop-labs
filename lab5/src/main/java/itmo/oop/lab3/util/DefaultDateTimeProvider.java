c8689cdc8478cb90dc341f5f04367c8e5c5bacee
Ilya Kuznetsov <ikuz2002@gmail.com>
2020-12-10 02:04:57 +0300

    fix: Use TimerTask instead of Thread with Thread.sleep

--- lab5/src/main/java/itmo/oop/lab3/util/DefaultDateTimeProvider.java
+++ lab5/src/main/java/itmo/oop/lab3/util/DefaultDateTimeProvider.java
@@ -3,32 +3,17 @@ package itmo.oop.lab3.util;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.function.Consumer;
 
 public class DefaultDateTimeProvider implements DateTimeProvider {
 
     private final List<Consumer<ZonedDateTime>> hooks = new ArrayList<>();
 
-    @SuppressWarnings("BusyWait")
     public DefaultDateTimeProvider() {
-        Thread dateWatcher = new Thread(() -> {
-            while (true) {
-                ZonedDateTime previous = currentDateTime();
-
-                while (previous.getDayOfWeek() == currentDateTime().getDayOfWeek()) {
-                    try {
-                        Thread.sleep(3_600_000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                synchronized (hooks) {
-                    for (var hook : hooks)
-                        hook.accept(currentDateTime());
-                }
-            }
-        });
+        Timer t = new Timer();
+        t.schedule(new DateCheckerTask(), 3_600_000, 3_600_000);
     }
 
     @Override
@@ -40,6 +25,24 @@ public class DefaultDateTimeProvider implements DateTimeProvider {
     public void addDateChangeHook(Consumer<ZonedDateTime> hook) {
         synchronized (hooks) {
             hooks.add(hook);
+        }
+    }
+
+    private class DateCheckerTask extends TimerTask {
+
+        private ZonedDateTime previous = currentDateTime();
+
+        @Override
+        public void run() {
+            if (previous.getDayOfWeek() == currentDateTime().getDayOfWeek())
+                return;
+
+            synchronized (hooks) {
+                for (var hook : hooks)
+                    hook.accept(currentDateTime());
+            }
+
+            previous = currentDateTime();
         }
     }
 }
